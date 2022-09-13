acabebf84d42597f42f42f5609e61c435b475183
Sarah Sami <sarah@Sarahs-MacBook-Pro.local>
2022-09-13 18:55:33 -0400

    callback done

--- callback_examples.js
+++ callback_examples.js
@@ -19,7 +19,29 @@ function sum(a,b){
     return c
 }
 
-// create a callback function that calls f1 and sum
+// create a callback function that calls f1 and sum -- this is actually not callback i wanted to see if it works this way or not
+// however with callback you add the function as a freaking variable, then when calling it you pass the function like -sum-
 function callback(f,x) {
-
+    if (x>0 && f>0) {
+        sum(x,f)
+    }
+    else {
+        console.log('dude')
+    }
 }
+
+callback(10,20)
+
+function testCallBack(x, success, fail) {
+    if (x==10) {
+        success()
+    } else {
+        fail()
+    }
+}
+
+testCallBack(100,
+    ()=>console.log("success called"),
+    ()=>console.log("fail called")
+    )
+
---
+++ promise_examples.js
@@ -0,0 +1,3 @@
+console.log("promise examples")
+
+var p1 = new Promise()
